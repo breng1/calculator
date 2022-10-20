ca7e4a18b3ad86dfa06d6f2287ff619423c9c90d
breng1 <brendaknguyen@gmail.com>
2022-10-19 19:22:32 -0700

    Add operator functions to script.js
    Implement add(), subtract(), multiply(), and divide().

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // script.js
+
+// Adds two numbers
+function add (a,b) {
+    return a + b;
+}
+
+// Subtracts two numbers
+function subtract (a, b) {
+    return a - b;
+}
+
+// Multiplies two numbers
+function multiply (a, b) {
+    return a * b;
+}
+
+// Divides two numbers 
+function divide (a,b) {
+    return a/b;
+}
